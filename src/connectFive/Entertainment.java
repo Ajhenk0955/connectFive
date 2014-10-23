084937a092d61c4db22ee47946f58603e0063321
rmpope89 <rmpope@eagle.fgcu.edu>
2014-10-23 17:32:52 -0400

    Prototype Ready Entertainment

--- src/connectFive/Entertainment.java
+++ src/connectFive/Entertainment.java
@@ -1,17 +1,20 @@
+package connectFive;
+
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+//import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
-// Original work started on 10/22
-// Author: Courtney Kuhlman
-
-class Frame extends JFrame{
+class Entertainment extends JFrame implements ActionListener{
 	private JButton submit;
 	private JRadioButton morning;
 	private JRadioButton afternoon;
@@ -19,7 +22,7 @@ class Frame extends JFrame{
 	private JRadioButton sunny;
 	private JRadioButton cloudy;
 	private JRadioButton rainy;
-	private JRadioButton stroming;
+	private JRadioButton storming;
 	private JRadioButton allDay;
 	private JRadioButton twoHours;
 	private JRadioButton twoFiveH;
@@ -44,17 +47,18 @@ class Frame extends JFrame{
 	private JLabel age;
 	private JLabel distance;
 	private ButtonGroup radioGroup;
+	private ButtonGroup radioGroup1;
+	int [] array = new int [7];
 	
-	Frame (String test){
+	Entertainment (String test){
 		super(test);
+		JPanel button = new JPanel(new GridLayout(0,1));
 		JPanel buttonJPanel = new JPanel();
-		buttonJPanel.setLayout(new FlowLayout());
 		submit = new JButton("Submit");
 		buttonJPanel.add(submit);
 		add(buttonJPanel, BorderLayout.SOUTH);
-		JPanel button = new JPanel();
-		JPanel question = new JPanel();
-		button.setLayout(new FlowLayout());
+		
+		//question 1
 		time = new JLabel ("What is the time of the day?");
 		morning = new JRadioButton("morning");
 		afternoon = new JRadioButton("afternoon");
@@ -63,81 +67,260 @@ class Frame extends JFrame{
 		button.add(morning);
 		button.add(afternoon);
 		button.add(night);
-		radioGroup = new ButtonGroup();
-		radioGroup.add(morning);
-		radioGroup.add(afternoon);
-		radioGroup.add(night);
+		radioGroup1 = new ButtonGroup();
+		radioGroup1.add(morning);
+		radioGroup1.add(afternoon);
+		radioGroup1.add(night);
 		add(button);
-		//new
-		JPanel button1 = new JPanel();
-		//JPanel question1 = new JPanel();
-		button.setLayout(new FlowLayout());
-		time = new JLabel ("What is the time of the day?");
-		morning = new JRadioButton("morning");
-		afternoon = new JRadioButton("afternoon");
-		night = new JRadioButton("night");
-		button1.add(time);
-		button1.add(morning);
-		button1.add(afternoon);
-		button1.add(night);
-		radioGroup = new ButtonGroup();
-		radioGroup.add(morning);
-		radioGroup.add(afternoon);
-		radioGroup.add(night);
-		add(button1, BorderLayout.CENTER);
-	}
-}
-public class Entertainment extends JPanel{
-	private JRadioButton morning;
-	private JRadioButton afternoon;
-	private JRadioButton night;
-	private JRadioButton sunny;
-	private JRadioButton cloudy;
-	private JRadioButton rainy;
-	private JRadioButton stroming;
-	private JRadioButton allDay;
-	private JRadioButton twoHours;
-	private JRadioButton twoFiveH;
-	private JRadioButton indoor;
-	private JRadioButton outdoor;
-	private JRadioButton lessThan;
-	private JRadioButton tenTwenty;
-	private JRadioButton TwentyFifty;
-	private JRadioButton fiftyPlus;
-	private JRadioButton young;
-	private JRadioButton eighteenTwenty;
-	private JRadioButton twentyOnePlus;
-	private JRadioButton fiveMiles;
-	private JRadioButton fiveFifteen;
-	private JRadioButton fifteenThirty;
-	private JRadioButton thirtyPlus;
-	private JLabel time;
-	private JLabel weather;
-	private JLabel length;
-	private JLabel inOrOut;
-	private JLabel cost;
-	private JLabel age;
-	private JLabel distance;
-	
-	public Entertainment(){
-		JFrame window=new JFrame();
-		time = new JLabel ("What is the time of the day?");
-		morning = new JRadioButton("morning");
-		afternoon = new JRadioButton("afternoon");
-		night = new JRadioButton("night");
-		window.add(time);
-		window.add(morning);
-		window.add(afternoon);
-		window.add(night);
-		sunny = new JRadioButton("sunny");
 		
+		//question 2
+		weather = new JLabel ("What is the weather like?");
+		sunny = new JRadioButton("sunny");
+		cloudy = new JRadioButton("cloudy");
+		rainy = new JRadioButton("rainy");
+		storming = new JRadioButton("storming");
+		button.add(weather);
+		button.add(sunny);
+		button.add(cloudy);
+		button.add(rainy);
+		button.add(storming);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(sunny);
+		radioGroup.add(cloudy);
+		radioGroup.add(rainy);
+		radioGroup.add(storming);
+		add(button);
+		
+		//question 3
+		length = new JLabel ("How long do you want to be out for?");
+		allDay = new JRadioButton("all day");
+		twoHours = new JRadioButton("less than 2 hours");
+		twoFiveH = new JRadioButton("2-5 hours");
+		button.add(length);
+		button.add(allDay);
+		button.add(twoHours);
+		button.add(twoFiveH);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(allDay);
+		radioGroup.add(twoHours);
+		radioGroup.add(twoFiveH);
+		add(button);
+		
+		//question 4
+		inOrOut = new JLabel ("Indoor or outdoor?");
+		indoor = new JRadioButton("indoor");
+		outdoor = new JRadioButton("outdoor");
+		button.add(inOrOut);
+		button.add(indoor);
+		button.add(outdoor);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(indoor);
+		radioGroup.add(outdoor);
+		add(button);
+		
+		//question 5
+		cost = new JLabel ("How much money can you spend?");
+		lessThan = new JRadioButton("less than $10");
+		tenTwenty = new JRadioButton("$10-20");
+		TwentyFifty = new JRadioButton("$20-50");
+		fiftyPlus = new JRadioButton("$50+");
+		button.add(cost);
+		button.add(lessThan);
+		button.add(tenTwenty);
+		button.add(TwentyFifty);
+		button.add(fiftyPlus);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(lessThan);
+		radioGroup.add(tenTwenty);
+		radioGroup.add(TwentyFifty);
+		radioGroup.add(fiftyPlus);
+		add(button);
+		
+		//question 6
+		age = new JLabel ("How old are you?");
+		young = new JRadioButton("younger than 18");
+		eighteenTwenty = new JRadioButton("18-20");
+		twentyOnePlus = new JRadioButton("21+");
+		button.add(age);
+		button.add(young);
+		button.add(eighteenTwenty);
+		button.add(twentyOnePlus);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(young);
+		radioGroup.add(eighteenTwenty);
+		radioGroup.add(twentyOnePlus);
+		add(button);
+		
+		//question 7
+		distance = new JLabel ("How far can you travel?");
+		fiveMiles = new JRadioButton("0-5 miles");
+		fiveFifteen = new JRadioButton("5-15 miles");
+		fifteenThirty = new JRadioButton("15-30 miles");
+		thirtyPlus = new JRadioButton("30+ miles");
+		button.add(distance);
+		button.add(fiveMiles);
+		button.add(fiveFifteen);
+		button.add(fifteenThirty);
+		button.add(thirtyPlus);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(fiveMiles);
+		radioGroup.add(fiveFifteen);
+		radioGroup.add(fifteenThirty);
+		radioGroup.add(thirtyPlus);
+		add(button);
+		
+		// action listeners
+		submit.addActionListener(this);
+		morning.addActionListener(this);
+		afternoon.addActionListener(this);
+		night.addActionListener(this);
+		sunny.addActionListener(this);
+		cloudy.addActionListener(this);
+		rainy.addActionListener(this);
+		storming.addActionListener(this);
+		allDay.addActionListener(this);
+		twoHours.addActionListener(this);
+		twoFiveH.addActionListener(this);
+		indoor.addActionListener(this);
+		outdoor.addActionListener(this);
+		lessThan.addActionListener(this);
+		tenTwenty.addActionListener(this);
+		TwentyFifty.addActionListener(this);
+		fiftyPlus.addActionListener(this);
+		young.addActionListener(this);
+		eighteenTwenty.addActionListener(this);
+		twentyOnePlus.addActionListener(this);
+		fiveMiles.addActionListener(this);
+		fiveFifteen.addActionListener(this);
+		fifteenThirty.addActionListener(this);
+		thirtyPlus.addActionListener(this);
+		
+		// array to store results
+		int [] maxArray = new int [7];
+		maxArray[0] = 2;
+		maxArray[1] = 3;
+		maxArray[2] = 2;
+		maxArray[3] = 1;
+		maxArray[4] = 3;
+		maxArray[5] = 2;
+		maxArray[6] = 3;
 	}
 	
+	public void actionPerformed(ActionEvent e){
+		
+		if(e.getSource() == morning){
+			array[0] = 0;
+		}
+		
+		else if(e.getSource() == afternoon){
+			array[0] = 1;
+		}
+		
+		else if(e.getSource() == night){
+			array[0] = 2;
+		}
+		
+		if(e.getSource() == sunny){
+			array[1] = 0;
+		}
+		
+		else if(e.getSource() == cloudy){
+			array[1] = 1;
+		}
+		
+		else if(e.getSource() == rainy){
+			array[1] = 2;
+		}
+		
+		else if(e.getSource() == storming){
+			array[1] = 3;
+		}
+		
+		if(e.getSource() == allDay){
+			array[2] = 0;
+		}
+		
+		else if(e.getSource() == twoHours){
+			array[2] = 1;
+		}
+		
+		else if(e.getSource() == twoFiveH){
+			array[2] = 2;
+		}
+		
+		if(e.getSource() == indoor){
+			array[3] = 0;
+		}
+		
+		else if(e.getSource() == outdoor){
+			array[3] = 1;
+		}
+		
+		if(e.getSource() == lessThan){
+			array[4] = 0;
+		}
+		
+		else if(e.getSource() == tenTwenty){
+			array[4] = 1;
+		}
+		
+		else if(e.getSource() == TwentyFifty){
+			array[4] = 2;
+		}
+		
+		else if(e.getSource() == fiftyPlus){
+			array[4] = 3;
+		}
+		
+		if(e.getSource() == young){
+			array[5] = 0;
+		}
+		
+		else if(e.getSource() == eighteenTwenty){
+			array[5] = 1;
+		}
+		
+		else if(e.getSource() == twentyOnePlus){
+			array[5] = 2;
+		}
+		
+		if(e.getSource() == fiveMiles){
+			array[6] = 0;
+		}
+		
+		else if(e.getSource() == fiveFifteen){
+			array[6] = 1;
+		}
+		
+		else if(e.getSource() == fifteenThirty){
+			array[6] = 2;
+		}
+		
+		else if(e.getSource() == thirtyPlus){
+			array[6] = 3;
+		}
+		
+		// testing result storage
+		System.out.println("1: " + array[0]);
+		System.out.println("2: " + array[1]);
+		System.out.println("3: " + array[2]);
+		System.out.println("4: " + array[3]);
+		System.out.println("5: " + array[4]);
+		System.out.println("6: " + array[5]);
+		System.out.println("7: " + array[6]);
+		
+		if(e.getSource() == submit){
+			JOptionPane.showMessageDialog(null, "You've submitted your test!");
+			this.setVisible(false);
+		}
+	}
 	
+
 	public static void main( String args[] ){
-		   Frame buttonFrame = new Frame("test");
+		   Entertainment buttonFrame = new Entertainment("test");
 		   buttonFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		   buttonFrame.setSize( 360, 640 ); // set frame size
 		   buttonFrame.setVisible( true ); // display frame
 	}
+
 }
