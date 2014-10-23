e984dcc7e858f80d4c383b5ba7d3d26d820988c2
ajhenk <Ajhenk0955@eagle.fgcu.edu>
2014-10-23 00:56:58 -0400

    javaml-0.1.7 & KD-Trees
    Added a bunch of classes that we can use.

--- src/connectFive/dataManager.java
+++ src/connectFive/dataManager.java
@@ -2,31 +2,54 @@ package connectFive;
 
 import java.awt.Point;
 
+import net.sf.javaml.core.kdtree.KDTree;
+
 public class dataManager {
 	//nearest neighbor search
 	//closest point search
 	//K-D tree
 	//perfect! ill learn and create a k-d tree
-	//might make array a list
-	private Point[] locations;
-
-	public dataManager(){
-		getPoints();
-	}
-	public void calculatePoint(String name, int lowLimit, int highLimit, int value, boolean checker){
-		//given data create the point
-	}
-	public void getPoints(){
-		//for retrieving data stored on dataBase
-	}
-	public void addPoints(){
-		//for adding a user or a functionality point
-	}
-	public void removePoints(){
-		//removing user or a functionality point
-	}
-	public void findNearestPoints(Point user, int radius, Point[] results){
-		//returns all the points within a selected radius
-	}
+	//kdTree involves created nodes
 	
+	//might make array a list
+	private KDTree tree;
+
+	public dataManager(int questions){
+		tree = new KDTree(questions);
+	}
+	/*below does show an incomplete object
+	//we should create a questionnaire object for 
+	//keeping it simple
+	//object could contain:
+	//UserID, number of questions, questionResponses(on a scale)
+	//maybe an array: length would be number of questions
+	//value at array[i] would be the answer on a scale of (10?)
+	*/
+	public void addUserNode(object userAnswers){
+		//find the nodeKey and place node in KD-Tree
+		double[] key;
+		generateKey(userAnswers, key);
+		
+		tree.insert(key, userAnswers);
+	}
+	public void generateKey(object userAnswers, double[] key){
+		//find key somehow
+	}
+	public void deleteNode(object userAnswers){
+		//removes data point and rebuilds structure
+		double[] key;
+		generateKey(userAnswers, key);
+		tree.delete(key);
+		rebuild();
+	}
+	public void rebuild(){
+		//rebuilds the structure?!
+	}
+	public object[] getNearest(object userAnswers){
+		double[] key;
+		generateKey(userAnswers, key);
+		
+		//set to 4 closest nodes
+		return tree.nearest(key, 4);
+	}
 }
