4e02d8dcdd58feac0bd13b8d6bdb44924404cff9
rmpope89 <rmpope@eagle.fgcu.edu>
2014-10-24 15:57:19 -0400

    Carpool and Restaurant Questionnaires

---
+++ src/connectFive/Carpool.java
@@ -0,0 +1,303 @@
+package connectFive;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Carpool extends JFrame implements ActionListener{
+	private JButton submit;
+	private JRadioButton give;
+	private JRadioButton receive;
+	
+	private JRadioButton me;
+	private JRadioButton two;
+	private JRadioButton three;
+	private JRadioButton four;
+	
+	private JRadioButton female;
+	private JRadioButton male;
+	private JRadioButton none;
+	
+	private JRadioButton on;
+	private JRadioButton close;
+	private JRadioButton less5;
+	private JRadioButton more5;
+	
+	private JRadioButton on2;
+	private JRadioButton close2;
+	private JRadioButton less52;
+	private JRadioButton more52;
+	
+	private JRadioButton now;
+	private JRadioButton fifteen;
+	private JRadioButton thirty;
+	private JRadioButton hour;
+	private JRadioButton hourP;
+	
+	private JRadioButton fifteen2;
+	private JRadioButton thirty2;
+	private JRadioButton hour2;
+	private JRadioButton hourP2;
+	
+	private JLabel look;
+	private JLabel party;
+	private JLabel prefer;
+	private JLabel whereAt;
+	private JLabel whereTo;
+	private JLabel depart;
+	private JLabel arrive;
+	private ButtonGroup radioGroup;
+
+	int [] array = new int [7];
+	
+	Carpool (){
+		super("Carpool Questionnaire");
+		JPanel button = new JPanel(new GridLayout(0,1));
+		JPanel buttonJPanel = new JPanel();
+		submit = new JButton("Submit");
+		buttonJPanel.add(submit);
+		add(buttonJPanel, BorderLayout.SOUTH);
+		
+		//question 1
+		look = new JLabel ("1) Are you looking to:");
+		give = new JRadioButton("Give a ride");
+		receive = new JRadioButton("Receive a ride");
+		button.add(look);
+		button.add(give);
+		button.add(receive);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(give);
+		radioGroup.add(receive);
+		add(button);
+		
+		//question 2
+		party = new JLabel ("2) How many people are in your party?");
+		me = new JRadioButton("Just me");
+		two  = new JRadioButton("Two");
+		three = new JRadioButton("Three");
+		four = new JRadioButton("Four");
+		button.add(party);
+		button.add(me);
+		button.add(two);
+		button.add(three);
+		button.add(four);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(me);
+		radioGroup.add(two);
+		radioGroup.add(three);
+		radioGroup.add(four);
+		add(button);
+		
+		//question 3
+		prefer = new JLabel ("3) Who would you prefer to ride with?");
+		female = new JRadioButton("A female");
+		male = new JRadioButton("A male");
+		none = new JRadioButton("No preference");
+		button.add(prefer);
+		button.add(female);
+		button.add(male);
+		button.add(none);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(female);
+		radioGroup.add(male);
+		radioGroup.add(none);
+		add(button);
+		
+		//question 4
+		whereAt = new JLabel ("4) Where are you?");
+		on = new JRadioButton("On campus");
+		close = new JRadioButton("Within a mile of campus");
+		less5 = new JRadioButton("Less than five miles from campus");
+		more5 = new JRadioButton("More than five miles from campus");
+		button.add(whereAt);
+		button.add(on);
+		button.add(close);
+		button.add(less5);
+		button.add(more5);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(on);
+		radioGroup.add(close);
+		radioGroup.add(less5);
+		radioGroup.add(more5);
+		add(button);
+		
+		//question 5
+		whereTo = new JLabel ("5) Where are you going?");
+		on2 = new JRadioButton("On campus");
+		close2 = new JRadioButton("Within a mile of campus");
+		less52 = new JRadioButton("Less than five miles from campus");
+		more52 = new JRadioButton("More than five miles from campus");
+		button.add(whereTo);
+		button.add(on2);
+		button.add(close2);
+		button.add(less52);
+		button.add(more52);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(on2);
+		radioGroup.add(close2);
+		radioGroup.add(less52);
+		radioGroup.add(more52);
+		add(button);
+		
+		//question 6
+		depart = new JLabel ("6) What time do you need to depart?");
+		now = new JRadioButton("Now");
+		fifteen = new JRadioButton("15 minutes");
+		thirty = new JRadioButton("30 minutes");
+		hour = new JRadioButton("1 hour");
+		hourP = new JRadioButton("More than an hour");
+		button.add(depart);
+		button.add(now);
+		button.add(fifteen);
+		button.add(thirty);
+		button.add(hour);
+		button.add(hourP);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(now);
+		radioGroup.add(fifteen);
+		radioGroup.add(thirty);
+		radioGroup.add(hour);
+		radioGroup.add(hourP);
+		add(button);
+		
+		//question 7
+		arrive = new JLabel ("7) What time do you need to arrive?");
+		fifteen2 = new JRadioButton("15 minutes");
+		thirty2 = new JRadioButton("30 minutes");
+		hour2 = new JRadioButton("1 hour");
+		hourP2 = new JRadioButton("More than an hour");
+		button.add(arrive);
+		button.add(fifteen2);
+		button.add(thirty2);
+		button.add(hour2);
+		button.add(hourP2);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(fifteen2);
+		radioGroup.add(thirty2);
+		radioGroup.add(hour2);
+		radioGroup.add(hourP2);
+		add(button);
+		
+		// action listener
+		submit.addActionListener(this);
+		give.addActionListener(this);
+		receive.addActionListener(this);
+		me.addActionListener(this);
+		two.addActionListener(this);
+		three.addActionListener(this);
+		four.addActionListener(this);
+		female.addActionListener(this);
+		male.addActionListener(this);
+		none.addActionListener(this);
+		on.addActionListener(this);
+		close.addActionListener(this);
+		less5.addActionListener(this);
+		more5.addActionListener(this);
+		on2.addActionListener(this);
+		close2.addActionListener(this);
+		less52.addActionListener(this);
+		more52.addActionListener(this);
+		now.addActionListener(this);
+		fifteen.addActionListener(this);
+		thirty.addActionListener(this);
+		hour.addActionListener(this);
+		hourP.addActionListener(this);
+		fifteen2.addActionListener(this);
+		thirty2.addActionListener(this);
+		hour2.addActionListener(this);
+		hourP2.addActionListener(this);
+				
+	}
+	
+	
+	public void actionPerformed(ActionEvent e){
+		
+		if(e.getSource() == give){
+			array[0] = 0;
+		}else if(e.getSource() == receive){
+			array[0] = 1;
+		}
+		
+		if(e.getSource() == me){
+			array[1] = 0;
+		}else if(e.getSource() == two){
+			array[1] = 1;
+		}else if(e.getSource() == three){
+			array[1] = 2;
+		}else if(e.getSource() == four){
+			array[1] = 3;
+		}
+		
+		if(e.getSource() == female){
+			array[2] = 0;
+		}else if(e.getSource() == male){
+			array[2] = 1;
+		}else if(e.getSource() == none){
+			array[2] = 2;
+		}
+		
+		if(e.getSource() == on){
+			array[3] = 0;
+		}else if(e.getSource() == close){
+			array[3] = 1;
+		}else if(e.getSource() == less5){
+			array[3] = 2;
+		}else if(e.getSource() == more5){
+			array[3] = 3;
+		}
+
+		if(e.getSource() == on2){
+			array[4] = 0;
+		}else if(e.getSource() == close2){
+			array[4] = 1;
+		}else if(e.getSource() == less52){
+			array[4] = 2;
+		}else if(e.getSource() == more52){
+			array[4] = 3;
+		}
+		
+		if(e.getSource() == now){
+			array[5] = 0;
+		}else if(e.getSource() == fifteen){
+			array[5] = 1;
+		}else if(e.getSource() == thirty){
+			array[5] = 2;
+		}else if(e.getSource() == hour){
+			array[5] = 3;
+		}else if(e.getSource() == hourP){
+			array[5] = 4;
+		}
+		
+		if(e.getSource() == fifteen2){
+			array[6] = 0;
+		}else if(e.getSource() == thirty2){
+			array[6] = 1;
+		}else if(e.getSource() == hour2){
+			array[6] = 2;
+		}else if(e.getSource() == hourP2){
+			array[6] = 3;
+		}
+		
+		System.out.println("1: " + array[0]);
+		System.out.println("2: " + array[1]);
+		System.out.println("3: " + array[2]);
+		System.out.println("4: " + array[3]);
+		System.out.println("5: " + array[4]);
+		System.out.println("6: " + array[5]);
+		System.out.println("7: " + array[6]);
+		
+		if(e.getSource() == submit){
+			JOptionPane.showMessageDialog(null, "You've submitted your test!");
+			this.setVisible(false);
+		}
+	}
+	
+
+	public static void main( String args[] ){
+		   Carpool buttonFrame = new Carpool();
+		   buttonFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		   buttonFrame.setSize( 360, 640 ); // set frame size
+		   buttonFrame.setVisible( true ); // display frame
+	}
+
+}
---
+++ src/connectFive/Restaurant.java
@@ -0,0 +1,286 @@
+package connectFive;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Restaurant extends JFrame implements ActionListener{
+	private JButton submit;
+	private JRadioButton vegan;
+	private JRadioButton vegetarian;
+	private JRadioButton sea;
+	private JRadioButton gluten;
+	private JRadioButton none;
+	private JRadioButton african;
+	private JRadioButton asian;
+	private JRadioButton european;
+	private JRadioButton oceanic;
+	private JRadioButton americas;
+	private JRadioButton breakfast;
+	private JRadioButton lunch;
+	private JRadioButton dinner;
+	private JRadioButton morning;
+	private JRadioButton midday;
+	private JRadioButton afternoon;
+	private JRadioButton evening;
+	private JRadioButton late;
+	private JRadioButton less1;
+	private JRadioButton more1;
+	private JRadioButton less5;
+	private JRadioButton more5;
+	private JRadioButton zero;
+	private JRadioButton ten;
+	private JRadioButton twenty;
+	private JRadioButton thirty;
+	private JLabel type;
+	private JLabel cuisine;
+	private JLabel meal;
+	private JLabel time;
+	private JLabel travel;
+	private JLabel price;
+	private ButtonGroup radioGroup;
+
+	int [] array = new int [6];
+	
+	Restaurant (){
+		super("Restaurant Questionnaire");
+		JPanel button = new JPanel(new GridLayout(0,1));
+		JPanel buttonJPanel = new JPanel();
+		submit = new JButton("Submit");
+		buttonJPanel.add(submit);
+		add(buttonJPanel, BorderLayout.SOUTH);
+		
+		//question 1
+		type = new JLabel ("1) Which of these best describes you?");
+		vegan = new JRadioButton("Vegan");
+		vegetarian = new JRadioButton("Vegetarian");
+		sea = new JRadioButton("Seafood allergies");
+		gluten = new JRadioButton("Gluten intolerant");
+		none = new JRadioButton("No restrictions");
+		button.add(type);
+		button.add(vegan);
+		button.add(vegetarian);
+		button.add(sea);
+		button.add(gluten);
+		button.add(none);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(vegan);
+		radioGroup.add(vegetarian);
+		radioGroup.add(sea);
+		radioGroup.add(gluten);
+		radioGroup.add(none);
+		add(button);
+		
+		//question 2
+		cuisine = new JLabel ("2) What type of cuisine are you in the mood for?");
+		african = new JRadioButton("African");
+		asian  = new JRadioButton("Asian");
+		european = new JRadioButton("European");
+		oceanic = new JRadioButton("Oceanic");
+		americas = new JRadioButton("Americas");
+		button.add(cuisine);
+		button.add(african);
+		button.add(asian);
+		button.add(european);
+		button.add(oceanic);
+		button.add(americas);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(african);
+		radioGroup.add(asian);
+		radioGroup.add(european);
+		radioGroup.add(oceanic);
+		radioGroup.add(americas);
+		add(button);
+		
+		//question 3
+		meal = new JLabel ("3) Which meal type are you hungry for?");
+		breakfast = new JRadioButton("Breakfast");
+		lunch = new JRadioButton("Lunch");
+		dinner = new JRadioButton("Dinner");
+		button.add(meal);
+		button.add(breakfast);
+		button.add(lunch);
+		button.add(dinner);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(breakfast);
+		radioGroup.add(lunch);
+		radioGroup.add(dinner);
+		add(button);
+		
+		//question 4
+		time = new JLabel ("4) What time are you planning to eat?");
+		morning = new JRadioButton("Morning");
+		midday = new JRadioButton("Midday");
+		afternoon = new JRadioButton("Afternoon");
+		evening = new JRadioButton("Evening");
+		late = new JRadioButton("Late night");
+		button.add(time);
+		button.add(morning);
+		button.add(midday);
+		button.add(afternoon);
+		button.add(evening);
+		button.add(late);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(morning);
+		radioGroup.add(midday);
+		radioGroup.add(afternoon);
+		radioGroup.add(evening);
+		radioGroup.add(late);
+		add(button);
+		
+		//question 5
+		travel = new JLabel ("5) How far are you willing to travel?");
+		less1 = new JRadioButton("Less than a mile");
+		more1 = new JRadioButton("More than a mile");
+		less5 = new JRadioButton("Less than five miles");
+		more5 = new JRadioButton("More than five miles");
+		button.add(travel);
+		button.add(less1);
+		button.add(more1);
+		button.add(less5);
+		button.add(more5);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(less1);
+		radioGroup.add(more1);
+		radioGroup.add(less5);
+		radioGroup.add(more5);
+		add(button);
+		
+		//question 6
+		price = new JLabel ("6) How much are you willing to spend on a meal?");
+		zero = new JRadioButton("$0 - $9");
+		ten = new JRadioButton("$10 - $19");
+		twenty = new JRadioButton("$20 - $29");
+		thirty = new JRadioButton("$30+");
+		button.add(price);
+		button.add(zero);
+		button.add(ten);
+		button.add(twenty);
+		button.add(thirty);
+		radioGroup = new ButtonGroup();
+		radioGroup.add(zero);
+		radioGroup.add(ten);
+		radioGroup.add(twenty);
+		radioGroup.add(thirty);
+		add(button);
+				
+		// action listener
+		submit.addActionListener(this);
+		vegan.addActionListener(this);
+		vegetarian.addActionListener(this);
+		sea.addActionListener(this);
+		gluten.addActionListener(this);
+		none.addActionListener(this);
+		african.addActionListener(this);
+		asian.addActionListener(this);
+		european.addActionListener(this);
+		oceanic.addActionListener(this);
+		americas.addActionListener(this);
+		breakfast.addActionListener(this);
+		lunch.addActionListener(this);
+		dinner.addActionListener(this);
+		morning.addActionListener(this);
+		midday.addActionListener(this);
+		afternoon.addActionListener(this);
+		evening.addActionListener(this);
+		late.addActionListener(this);
+		less1.addActionListener(this);
+		more1.addActionListener(this);
+		less5.addActionListener(this);
+		more5.addActionListener(this);
+		zero.addActionListener(this);
+		ten.addActionListener(this);
+		twenty.addActionListener(this);
+		thirty.addActionListener(this);
+		
+	}
+	
+	
+	public void actionPerformed(ActionEvent e){
+		
+		if(e.getSource() == vegan){
+			array[0] = 0;
+		}else if(e.getSource() == vegetarian){
+			array[0] = 1;
+		}else if(e.getSource() == sea){
+			array[0] = 2;
+		}else if(e.getSource() == gluten){
+			array[0] = 3;
+		}else if(e.getSource() == none){
+			array[0] = 4;
+		}
+		
+		if(e.getSource() == african){
+			array[1] = 0;
+		}else if(e.getSource() == asian){
+			array[1] = 1;
+		}else if(e.getSource() == european){
+			array[1] = 2;
+		}else if(e.getSource() == oceanic){
+			array[1] = 3;
+		}else if(e.getSource() == americas){
+			array[1] = 4;
+		}
+		
+		if(e.getSource() == breakfast){
+			array[2] = 0;
+		}else if(e.getSource() == lunch){
+			array[2] = 1;
+		}else if(e.getSource() == dinner){
+			array[2] = 2;
+		}
+		
+		if(e.getSource() == morning){
+			array[3] = 0;
+		}else if(e.getSource() == midday){
+			array[3] = 1;
+		}else if(e.getSource() == afternoon){
+			array[3] = 2;
+		}else if(e.getSource() == evening){
+			array[3] = 3;
+		}else if(e.getSource() == late){
+			array[3] = 4;
+		}
+		
+		if(e.getSource() == less1){
+			array[4] = 0;
+		}else if(e.getSource() == more1){
+			array[4] = 1;
+		}else if(e.getSource() == less5){
+			array[4] = 2;
+		}else if(e.getSource() == more5){
+			array[4] = 3;
+		}
+		
+		if(e.getSource() == zero){
+			array[5] = 0;
+		}else if(e.getSource() == ten){
+			array[5] = 1;
+		}else if(e.getSource() == twenty){
+			array[5] = 2;
+		}else if(e.getSource() == thirty){
+			array[5] = 3;
+		}
+				
+		System.out.println("1: " + array[0]);
+		System.out.println("2: " + array[1]);
+		System.out.println("3: " + array[2]);
+		System.out.println("4: " + array[3]);
+		System.out.println("5: " + array[4]);
+		System.out.println("6: " + array[5]);
+		
+		if(e.getSource() == submit){
+			JOptionPane.showMessageDialog(null, "You've submitted your test!");
+			this.setVisible(false);
+		}
+	}
+	
+
+	public static void main( String args[] ){
+		   Restaurant buttonFrame = new Restaurant();
+		   buttonFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		   buttonFrame.setSize( 360, 640 ); // set frame size
+		   buttonFrame.setVisible( true ); // display frame
+	}
+
+}
